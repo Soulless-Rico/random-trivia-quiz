d69192fe993c3d01e353c739646eb0d37c1d743c
matusmasny <matus.masny@spsehalova.sk>
2025-11-28 15:34:18 +0100

    ide trochu
    ide trochu

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 
 
-async function fetctQuestions() {
-    const response = await fetch("https://opentdb.com/api.php?amount=10");
+async function fetchQuestions() {
+    const response = await fetch("https://opentdb.com/api.php?amount=1");
 
     if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`)
     }
 
     const data = await response.json();
+    const result = data.results[0];
+
+    const difficulty = data.results[0].difficulty;
+    const category = data.results[0].category;
+    const question = data.results[0].question;
+
+    const answers = [...result.incorrect_answers, result.correct_answer];
+
+
+
+const container = document.getElementById("quiz");
+container.innerHTML = `
+    <h2>Difficulty: ${difficulty}</h2>
+    <h2>Category: ${category}</h2>
+    <h2>Question: ${question}</h2>
+    <h2>Answers: ${answers}</h2>
+`;
+
 
 
 }
+
+
+fetchQuestions()
